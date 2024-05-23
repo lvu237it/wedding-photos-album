9e5dcde3e50bbe9981f77d3f760b977c2f346b8a
lvu237it <luuvanvua7k16vt@gmail.com>
2024-05-23 11:13:10 +0700

    Demo apply CI/CD after successfully build and deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AboutMe from './components/AboutMe'
+// import AboutMe from './components/AboutMe'
 function App() {
 
   return (
@@ -11,7 +11,7 @@ function App() {
       <Header />
         <Routes>
           <Route path="/" element={<Body />} />
-          <Route path="/about" element={<AboutMe />} />
+          {/* <Route path="/about" element={<AboutMe />} /> */}
         </Routes>
       <Footer />
       </BrowserRouter>
